ff751dfe6a2795f6f62314d5e85721fa8257d1a8
yichenlin <yichenlin@creality.com>
2023-07-24 19:31:14 +0800

    fix[add color ]input step files
    Change-Id: Ia29f48b508ef107ba53a5ce230964fba5f97fc49

--- cadcore/step/STEP.cpp
+++ cadcore/step/STEP.cpp
@@ -27,11 +27,172 @@
 #include "TopExp_Explorer.hxx"
 #include "BRep_Tool.hxx"
 
+#include "XCAFDoc_ColorType.hxx"
+#include "Quantity_Color.hxx"
+#include "XCAFDoc_DocumentTool.hxx"
+#include "XCAFDoc_ColorTool.hxx"
+#include "Quantity_ColorRGBA.hxx"
+#include "TopTools_IndexedMapOfShape.hxx"
+#include <map>
 const double STEP_TRANS_CHORD_ERROR = 0.0025;
 const double STEP_TRANS_ANGLE_RES = 0.5;
 
 
 namespace Slic3r {
+
+    class  Color
+    {
+    public:
+        /**
+         * Defines the color as (R,G,B,A) whereas all values are in the range [0,1].
+         * \a A defines the transparency.
+         */
+        explicit Color(float R = 0.0, float G = 0.0, float B = 0.0, float A = 0.0)
+            :r(R), g(G), b(B), a(A) {}
+        /**
+         * Does basically the same as the constructor above unless that (R,G,B,A) is
+         * encoded as an unsigned int.
+         */
+        Color(uint32_t rgba)
+        {
+            setPackedValue(rgba);
+        }
+        /** Copy constructor. */
+        Color(const Color& c)
+            :r(c.r), g(c.g), b(c.b), a(c.a) {}
+        /** Returns true if both colors are equal. Therefore all components must be equal. */
+        bool operator==(const Color& c) const
+        {
+            return getPackedValue() == c.getPackedValue();
+            //return (c.r==r && c.g==g && c.b==b && c.a==a);
+        }
+        bool operator!=(const Color& c) const
+        {
+            return !operator==(c);
+        }
+        /**
+         * Defines the color as (R,G,B,A) whereas all values are in the range [0,1].
+         * \a A defines the transparency, 0 means complete opaque and 1 invisible.
+         */
+        void set(float R, float G, float B, float A = 0.0)
+        {
+            r = R; g = G; b = B; a = A;
+        }
+        Color& operator=(const Color& c)
+        {
+            r = c.r; g = c.g; b = c.b; a = c.a;
+            return *this;
+        }
+        /**
+         * Sets the color value as a 32 bit combined red/green/blue/alpha value.
+         * Each component is 8 bit wide (i.e. from 0x00 to 0xff), and the red
+         * value should be stored leftmost, like this: 0xRRGGBBAA.
+         *
+         * \sa getPackedValue().
+         */
+        Color& setPackedValue(uint32_t rgba)
+        {
+            this->set((rgba >> 24) / 255.0f,
+                ((rgba >> 16) & 0xff) / 255.0f,
+                ((rgba >> 8) & 0xff) / 255.0f,
+                (rgba & 0xff) / 255.0f);
+            return *this;
+        }
+        /**
+         * Returns color as a 32 bit packed unsigned int in the form 0xRRGGBBAA.
+         *
+         *  \sa setPackedValue().
+         */
+        uint32_t getPackedValue() const
+        {
+            return ((uint32_t)(r * 255.0f + 0.5f) << 24 |
+                (uint32_t)(g * 255.0f + 0.5f) << 16 |
+                (uint32_t)(b * 255.0f + 0.5f) << 8 |
+                (uint32_t)(a * 255.0f + 0.5f));
+        }
+        /**
+         * creates FC Color from template type, e.g. Qt QColor
+         */
+        template <typename T>
+        void setValue(const T& q)
+        {
+            set(q.redF(), q.greenF(), q.blueF());
+        }
+        /**
+         * returns a template type e.g. Qt color equivalent to FC color
+         *
+         */
+        template <typename T>
+        inline T asValue(void) const {
+            return(T(int(r * 255.0), int(g * 255.0), int(b * 255.0)));
+        }
+        /**
+         * returns color as hex color "#RRGGBB"
+         *
+         */
+        std::string asHexString() const {
+            std::stringstream ss;
+            ss << "#" << std::hex << std::uppercase << std::setfill('0') << std::setw(2) << int(r * 255.0)
+                << std::setw(2) << int(g * 255.0)
+                << std::setw(2) << int(b * 255.0);
+            return ss.str();
+        }
+        /**
+         * \deprecated
+         */
+        std::string asCSSString() const {
+            return asHexString();
+        }
+        /**
+         * gets color from hex color "#RRGGBB"
+         *
+         */
+        bool fromHexString(const std::string& hex) {
+            if (hex.size() < 7 || hex[0] != '#')
+                return false;
+            // #RRGGBB
+            if (hex.size() == 7) {
+                std::stringstream ss(hex);
+                unsigned int rgb;
+                char c;
+
+                ss >> c >> std::hex >> rgb;
+                int rc = (rgb >> 16) & 0xff;
+                int gc = (rgb >> 8) & 0xff;
+                int bc = rgb & 0xff;
+
+                r = rc / 255.0f;
+                g = gc / 255.0f;
+                b = bc / 255.0f;
+
+                return true;
+            }
+            // #RRGGBBAA
+            if (hex.size() == 9) {
+                std::stringstream ss(hex);
+                unsigned int rgba;
+                char c;
+
+                ss >> c >> std::hex >> rgba;
+                int rc = (rgba >> 24) & 0xff;
+                int gc = (rgba >> 16) & 0xff;
+                int bc = (rgba >> 8) & 0xff;
+                int ac = rgba & 0xff;
+
+                r = rc / 255.0f;
+                g = gc / 255.0f;
+                b = bc / 255.0f;
+                a = ac / 255.0f;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        /// color values, public accessible
+        float r, g, b, a;
+    };
 
 //bool StepPreProcessor::preprocess(const char* path, std::string &output_path)
 //{
@@ -168,7 +329,7 @@ struct NamedSolid {
 
 static void getNamedSolids(const TopLoc_Location& location, const std::string& prefix,
                            unsigned int& id, const Handle(XCAFDoc_ShapeTool) shapeTool,
-                           const TDF_Label label, std::vector<NamedSolid>& namedSolids) {
+                           const TDF_Label label, std::vector<NamedSolid>& namedSolids, std::vector<TopLoc_Location>&namedLables) {
     TDF_Label referredLabel{label};
     if (shapeTool->IsReference(label))
         shapeTool->GetReferredShape(label, referredLabel);
@@ -186,9 +347,10 @@ static void getNamedSolids(const TopLoc_Location& location, const std::string& p
     TDF_LabelSequence components;
     if (shapeTool->GetComponents(referredLabel, components)) {
         for (Standard_Integer compIndex = 1; compIndex <= components.Length(); ++compIndex) {
-            getNamedSolids(localLocation, fullName, id, shapeTool, components.Value(compIndex), namedSolids);
+            getNamedSolids(localLocation, fullName, id, shapeTool, components.Value(compIndex), namedSolids, namedLables);
         }
     } else {
+        namedLables.push_back(localLocation);
         TopoDS_Shape shape;
         shapeTool->GetShape(referredLabel, shape);
         TopAbs_ShapeEnum shape_type = shape.ShapeType();
@@ -225,6 +387,7 @@ trimesh::TriMesh* load_step(const char *path,  ccglobal::Tracer* tracer)
     std::string file_after_preprocess = std::string(path);
 
     std::vector<NamedSolid> namedSolids;
+    std::vector<TopLoc_Location> namedLables;
     Handle(TDocStd_Document) document;
     Handle(XCAFApp_Application) application = XCAFApp_Application::GetApplication();
     application->NewDocument(file_after_preprocess.c_str(), document);
@@ -243,10 +406,60 @@ trimesh::TriMesh* load_step(const char *path,  ccglobal::Tracer* tracer)
     Handle(XCAFDoc_ShapeTool) shapeTool = XCAFDoc_DocumentTool::ShapeTool(document->Main());
     TDF_LabelSequence topLevelShapes;
     shapeTool->GetFreeShapes(topLevelShapes);
-
+    int AXXX = topLevelShapes.Length();
     unsigned int id{1};
     Standard_Integer topShapeLength = topLevelShapes.Length() + 1;
     auto stage_unit2 = topShapeLength / LOAD_STEP_STAGE_UNIT_NUM + 1;
+
+    Handle(XCAFDoc_ColorTool) rootColorTool = XCAFDoc_DocumentTool::ColorTool(document->Main());
+    TDF_Label  lab(document->Main().FindChild(0,Standard_True));
+    //Quantity_ColorRGBA col();
+    //(const Quantity_Color & col, TDF_Label & lab) const;
+    Quantity_ColorRGBA colRGBA;
+    Quantity_Color col;
+    //lab = rootColorTool->FindColor(col);
+    //rootColorTool.GetColor();
+
+
+    TDF_LabelSequence tdfLabels;
+    shapeTool->GetShapes(tdfLabels);   //获取装配体和组件对应名称
+    int Roots = tdfLabels.Length();
+    Color color(0.8f, 0.8f, 0.8f);
+    std::map <std::string, Color> colors;
+    //std::vector<Color> faceColors;
+    for (int i = 1; i <= Roots; i++)
+    {
+        TDF_Label label = tdfLabels.Value(i);
+        bool colorcolor = rootColorTool->IsSet(label, XCAFDoc_ColorGen);
+        bool colorcolor1 = rootColorTool->IsSet(label, XCAFDoc_ColorSurf);
+        bool colorcolor2 = rootColorTool->IsSet(label, XCAFDoc_ColorCurv);
+        //Standard_Boolean GetColor(const TopoDS_Shape & S, const XCAFDoc_ColorType type, TDF_Label & colorL);
+        Standard_Boolean xxsss5 = rootColorTool->GetColor(label, XCAFDoc_ColorGen, col);
+        Standard_Boolean xxsss6 = rootColorTool->GetColor(label, XCAFDoc_ColorSurf, col);
+        Standard_Boolean xxsss7 = rootColorTool->GetColor(label, XCAFDoc_ColorCurv, col);
+        color.r = (float)col.Red();
+        color.g = (float)col.Green();
+        color.b = (float)col.Blue();
+
+
+        TDF_Label referredLabel{ label };
+        if (shapeTool->IsReference(label))
+            shapeTool->GetReferredShape(label, referredLabel);
+
+        std::string name;
+        Handle(TDataStd_Name) shapeName;
+        if (referredLabel.FindAttribute(TDataStd_Name::GetID(), shapeName))
+            name = TCollection_AsciiString(shapeName->Get()).ToCString();
+
+        if (name == "")
+            name = std::to_string(id++);
+        std::string fullName{ name };
+        colors.emplace (name, color);
+    }
+
+
+
+
 
     for (Standard_Integer iLabel = 1; iLabel < topShapeLength; ++iLabel) {
         //if (stepFn) {
@@ -260,8 +473,11 @@ trimesh::TriMesh* load_step(const char *path,  ccglobal::Tracer* tracer)
         //        return false;
         //    }
         //}
-        getNamedSolids(TopLoc_Location{}, "", id, shapeTool, topLevelShapes.Value(iLabel), namedSolids);
+        getNamedSolids(TopLoc_Location{}, "", id, shapeTool, topLevelShapes.Value(iLabel), namedSolids, namedLables);
     }
+
+    std::vector<Color> color_submodule;
+    color_submodule.resize(namedSolids.size());
     if (tracer)
         tracer->progress(0.1f);
     std::vector<stl_file> stl;
@@ -298,11 +514,20 @@ trimesh::TriMesh* load_step(const char *path,  ccglobal::Tracer* tracer)
             Standard_Integer aNbFacesNoTri = 0;
             // BBS: fill temporary triangulation
             Standard_Integer aNodeOffset    = 0;
-            Standard_Integer aTriangleOffet = 0;
-            for (TopExp_Explorer anExpSF(namedSolids[i].solid, TopAbs_FACE); anExpSF.More(); anExpSF.Next()) {
+            Standard_Integer aTriangleOffet = 0; 
+            namedSolids[i].name;
+            TopoDS_Shape x = namedSolids[i].solid;
+            TopExp_Explorer anExpSF(namedSolids[i].solid, TopAbs_FACE);
+
+        //    Standard_Integer trait =  namedLables.at(i).;
+            Color clclclc = colors.at(namedSolids[i].name);
+            color_submodule.at(i) = clclclc;
+            while (anExpSF.More())
+            {
                 if (tracer)
                     tracer->progress(0.3f);
-                const TopoDS_Shape &aFace = anExpSF.Current();
+   
+                const TopoDS_Shape& aFace = anExpSF.Current();
                 TopLoc_Location     aLoc;
                 Handle(Poly_Triangulation) aTriangulation = BRep_Tool::Triangulation(TopoDS::Face(aFace), aLoc);
                 if (aTriangulation.IsNull()) {
@@ -353,6 +578,7 @@ trimesh::TriMesh* load_step(const char *path,  ccglobal::Tracer* tracer)
 
                 aNodeOffset += aTriangulation->NbNodes();
                 aTriangleOffet += aTriangulation->NbTriangles();
+                anExpSF.Next();
             }
         }
     });
@@ -367,13 +593,14 @@ trimesh::TriMesh* load_step(const char *path,  ccglobal::Tracer* tracer)
         tracer->failed("Parse File Error.");
 
 
-    trimesh::TriMesh * tm = new  trimesh::TriMesh();
+    trimesh::TriMesh* tm = new  trimesh::TriMesh();
     for (int k = 0; k < stl.size(); k++)
     {
         for (int i = 0; i < stl.at(k).facet_start.size(); i++)
         {
-           
-            stl_facet f =  stl.at(k).facet_start.at(i);
+            trimesh::Color c(color_submodule.at(k).r, color_submodule.at(k).g, color_submodule.at(k).b );
+            tm->colors.emplace_back(c);
+            stl_facet f = stl.at(k).facet_start.at(i);
             for (int j = 0; j < 3; j++)
             {
                 trimesh::point p(f.vertex[j].x(), f.vertex[j].y(), f.vertex[j].z());
@@ -393,6 +620,7 @@ trimesh::TriMesh* load_step(const char *path,  ccglobal::Tracer* tracer)
         tm->faces.push_back(face);
     }
     return tm;
+
 }
 
 }; // namespace Slic3r
